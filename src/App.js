6fdde79da931ebf24935782fc5c2f4332026dd23
Akshay G Dhole <akshaygdhole@gmail.com>
2022-05-11 15:41:08 +0530

    favicon updated

--- src/App.js
+++ src/App.js
@@ -1,49 +1,38 @@
 import './App.css';
-import React, { Component } from "react";
+import React, { useState } from "react";
 import NavBar from './Components/NavBar';
 import News from './Components/News';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar'
 
+const App = () => {
 
-export default class App extends Component {
+  const pageSize = 17
+  const apikey = process.env.REACT_APP_NEWS_API
+  const [progress, setProgress] = useState(0);
 
-  pageSize = 17
-  state = {
-    progress: 0
-  }
-  apikey = process.env.REACT_APP_NEWS_API
+  return <div>
+    <BrowserRouter>
+      <NavBar />
+      <LoadingBar
+        height={3}
+        color='#f11946'
+        progress={progress}
 
+      />
 
-  setProgress = (progress) => {
-    this.setState({ progress: progress })
-  }
+      <Routes>
+        <Route exact path="/general" element={<News setProgress={setProgress} apikey={apikey} key="general" pageSize={pageSize} country='in' category="general" />}> </Route>
+        <Route exact path="/entertainment" element={<News setProgress={setProgress} apikey={apikey} key="entertainment" pageSize={pageSize} country='in' category="entertainment" />} > </Route>
+        <Route exact path="/health" element={<News setProgress={setProgress} apikey={apikey} key="health" pageSize={pageSize} country='in' category="health" />} ></Route>
+        <Route exact path="/science" element={<News setProgress={setProgress} apikey={apikey} key="science" pageSize={pageSize} country='in' category="science" />}> </Route>
+        <Route exact path="/sports" element={<News setProgress={setProgress} apikey={apikey} key="sports" pageSize={pageSize} country='in' category="sports" />}></Route>
+        <Route exact path="/business" element={<News setProgress={setProgress} apikey={apikey} key="business" pageSize={pageSize} country='in' category="business" />}></Route>
+        <Route exact path="/technology" element={<News setProgress={setProgress} apikey={apikey} key="technology" pageSize={pageSize} country='in' category="technology" />}></Route>
+      </Routes>
+    </BrowserRouter>
+  </div>;
 
-  render() {
-    return <div>
-      <BrowserRouter>
-        <NavBar />
-        <LoadingBar
-          color='#f11946'
-          progress={this.state.progress}
-
-        />
-
-        <Routes>
-          <Route exact path="/general" element={<News setProgress={this.setProgress} apikey={this.apikey} key="general" pageSize={this.pageSize} country='in' category="general" />}> </Route>
-          <Route exact path="/entertainment" element={<News setProgress={this.setProgress} apikey={this.apikey} key="entertainment" pageSize={this.pageSize} country='in' category="entertainment" />} > </Route>
-          <Route exact path="/health" element={<News setProgress={this.setProgress} apikey={this.apikey} key="health" pageSize={this.pageSize} country='in' category="health" />} ></Route>
-          <Route exact path="/science" element={<News setProgress={this.setProgress} apikey={this.apikey} key="science" pageSize={this.pageSize} country='in' category="science" />}> </Route>
-          <Route exact path="/sports" element={<News setProgress={this.setProgress} apikey={this.apikey} key="sports" pageSize={this.pageSize} country='in' category="sports" />}></Route>
-          <Route exact path="/business" element={<News setProgress={this.setProgress} apikey={this.apikey} key="business" pageSize={this.pageSize} country='in' category="business" />}></Route>
-          <Route exact path="/technology" element={<News setProgress={this.setProgress} apikey={this.apikey} key="technology" pageSize={this.pageSize} country='in' category="technology" />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </div>;
-  }
 }
+export default App;
 
--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -1,12 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 import loading from "./loading.gif"
+const spinner = () => {
 
-export class spinner extends Component {
-    render() {
-        return <div className="text-center">
-            <img className="my-3" src={loading} alt="loading" />
-        </div>;
-    }
+    return <div className="text-center">
+        <img className="my-3" src={loading} alt="loading" />
+    </div>;
+
 }
 
 export default spinner;
